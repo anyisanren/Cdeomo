c7f2ccfaf6895d95a366708dd47ff8c69b731af2
Li Junyi <2228762641@qq.com>
2025-07-08 19:56:08 +0800

    Initial Commit

---
+++ Cdeomo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Cdeomo
+//
+//  Created by 李俊翊 on 2025/7/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
