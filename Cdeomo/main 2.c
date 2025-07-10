df7b520fe82720d879218f6aae94aeac15926060
Li Junyi <2228762641@qq.com>
2025-07-10 14:18:15 +0800

    Delete Cdeomo.xcodeproj directory

---
+++ Cdeomo/main 2.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Cdeomo
+//
+//  Created by 李俊翊 on 2025/7/8.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    printf("replaced")
+    return 0;
+}
